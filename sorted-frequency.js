ae4ef02c354ce1ef7a977888506ba8d4231195ec
Matthew Baer <matt.baer77@gmail.com>
2023-10-26 01:33:33 -0400

    sorted-forequency cleaned up

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,38 +1,4 @@
-// function sortedFrequency(sortedArr, val) {
 
-//     let leftIdx = 0;
-//     let rightIdx = sortedArr.length - 1;
-
-//     while(leftIdx <= rightIdx) {
-
-//         let middleIdx = Math.floor((leftIdx + rightIdx)/2)
-//         let leftCapIdx;
-//         let rightCapIdx;
-
-//         if (leftCapIdx && rightCapIdx) {
-
-//             return rightCapIdx - leftCapIdx
-//         }
-
-//         if (!leftCapIdx && sortedArr[middleIdx] === val && sortedArr[(middleIdx - 1)] != val) {
-
-//             leftCapIdx = middleIdx;
-
-//         }
-
-//         if (!rightCapIdx && sortedArr[middleIdx] === val && sortedArr[(middleIdx - 1)] != val) {
-
-//             rightCapIdx = middleIdx;
-
-//         }
-
-//         if (sortedArr[middleIdx] )
-
-
-
-//     }
-
-// }
 function sortedFrequency(sortedArr, val) {
 
     function findFirst() {
@@ -44,12 +10,7 @@ function sortedFrequency(sortedArr, val) {
 
             let middleIdx = Math.floor((leftIdx + rightIdx)/2)
 
-            console.log("INDEX: " + middleIdx)
-            console.log("VALUE: " + sortedArr[middleIdx])
-
             if(sortedArr[middleIdx] === val && sortedArr[(middleIdx - 1)] !== val) {
-
-                console.log(middleIdx)
 
                 return middleIdx
 
@@ -79,12 +40,7 @@ function sortedFrequency(sortedArr, val) {
 
             let middleIdx = Math.floor((leftIdx + rightIdx)/2)
 
-            console.log("INDEX: " + middleIdx)
-            console.log("VALUE: " + sortedArr[middleIdx])
-
             if(sortedArr[middleIdx] === val && sortedArr[(middleIdx + 1)] !== val) {
-
-                console.log(middleIdx)
 
                 return middleIdx
 
@@ -107,13 +63,8 @@ function sortedFrequency(sortedArr, val) {
     }
 
     const first = findFirst(sortedArr, val)
-    console.log(first)
     if (first === -1){return first}
     const last = findLast(sortedArr,val)
-    console.log(last)
-
-    console.log("DONE***")
-
     return last - first + 1
 
 }
