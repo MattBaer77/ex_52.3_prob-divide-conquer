dee11127adf8c33f64faecda6907534d7d5e3aab
Matthew Baer <matt.baer77@gmail.com>
2023-10-26 03:08:01 -0400

    completed find-floor

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,70 @@
-function findFloor() {
+function findFloor(sortedArr, val) {
+
+    let leftIdx = 0;
+    let rightIdx = sortedArr.length - 1;
+    let middleIdx;
+
+    while(leftIdx <= rightIdx) {
+
+        if (leftIdx === sortedArr.length - 1){
+            return sortedArr[(sortedArr.length -1)]
+        }
+
+        if (rightIdx === 0){
+            return -1
+        }
+
+        if (leftIdx === rightIdx) {
+            return sortedArr[(middleIdx)]
+        }
+
+        middleIdx = Math.floor((leftIdx + rightIdx)/2);
+        let middleVal = sortedArr[middleIdx]
+
+        if(middleVal === val){
+            return middleVal
+        }
+        else if(middleVal > val) {
+
+            console.log(middleIdx)
+            console.log(middleVal)
+
+            rightIdx = middleIdx - 1
+            
+        }
+        else if(middleVal < val) {
+
+            console.log(middleIdx)
+            console.log(middleVal)
+
+            leftIdx = middleIdx + 1
+
+        }
+
+    }
+
   
 }
+
+// ANSWER FROM SOLUTIONS NOTES -
+// function findFloor(arr, num, low = 0, high = arr.length - 1) {
+//     if (low > high) return -1;
+//     if (num >= arr[high]) return arr[high];
+  
+//     let mid = Math.floor((low + high) / 2)
+  
+//     if (arr[mid] === num) return arr[mid];
+  
+//     if (mid > 0 && arr[mid - 1] <= num && num < arr[mid]) {
+//       return arr[mid - 1];
+//     }
+  
+//     if (num < arr[mid]) {
+//       return findFloor(arr, num, low, mid - 1);
+//     }
+  
+//     return findFloor(arr, num, mid + 1, high)
+// }
+  
 
 module.exports = findFloor
--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,43 @@
-function findRotationCount() {
-  
-}
+// function findRotationCount() {
 
-module.exports = findRotationCount
+//     // I DO NOT EVEN UNDERSTAND THE QUESTION.
+  
+// }
+
+// module.exports = findRotationCount
+
+// ANSWER FROM SOLUTIONS NOTES -
+function findRotationCount(arr, low = 0, high = arr.length - 1) {
+
+    console.log(high)
+    console.log(low)
+    if (high < low) {
+
+        console.log("HIGH OVER LOW")
+
+        return 0;
+
+    } 
+    if (high === low) return low;
+    let mid = Math.floor((low + high) / 2)
+  
+    // Check if element (mid+1) is minimum element.
+    // Consider the cases like [3, 4, 5, 1, 2]
+    if (mid < high && arr[mid + 1] < arr[mid])
+      return mid + 1;
+  
+    // Check if mid itself is minimum element
+    if (mid > low && arr[mid] < arr[mid - 1]) {
+      return mid;
+    }
+  
+    // Decide whether we need to go to left half or
+    // right half
+    if (arr[high] > arr[mid]) {
+      return findRotationCount(arr, low, mid - 1);
+    }
+  
+    return findRotationCount(arr, mid + 1, high);
+  }
+  
+  module.exports = findRotationCount
