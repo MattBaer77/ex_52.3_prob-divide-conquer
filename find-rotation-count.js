2412cd4288360c95d2c1f0d918fa01974f3c8767
Matthew Baer <matt.baer77@gmail.com>
2023-10-30 23:04:35 -0400

    completed find-rotation-count

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,44 +1,73 @@
-// function findRotationCount() {
+function findRotationCount(sortedArr) {
 
-//     // I DO NOT EVEN UNDERSTAND THE QUESTION.
-//     // WILL DISCUSS WITH MENTOR
-  
-// }
+  // JUST WORK ON HALF OF THE ARRAY - ADJUST INDEXES ACCORDINGLY - ESPECIALLY IN WHILE LOOP
+  let leftIdx = 0
+  let rightIdx = sortedArr.length - 1
+
+  while(leftIdx <= rightIdx) {
+
+      let middleIdx = (Math.floor((leftIdx + rightIdx)/2));
+
+      console.log(leftIdx)
+      console.log(rightIdx)
+      console.log(middleIdx)
+
+      if(sortedArr[middleIdx] <= sortedArr[leftIdx] && sortedArr[middleIdx] < sortedArr[(middleIdx -1)]) {
+
+        console.log("FOUND!")
+        return middleIdx
+
+      } else if (sortedArr[middleIdx] < sortedArr[leftIdx]) {
+
+        console.log("MOVE LEFT")
+        rightIdx = middleIdx - 1
+
+      } else {
+
+        console.log("MOVE RIGHT")
+        leftIdx = middleIdx +1;
+
+      }
+
+  }
+  return 0
+
+}
 
 // module.exports = findRotationCount
 
 // ANSWER FROM SOLUTIONS NOTES -
-function findRotationCount(arr, low = 0, high = arr.length - 1) {
+// function findRotationCount(arr, low = 0, high = arr.length - 1) {
 
-    console.log(high)
-    console.log(low)
-    if (high < low) {
+//     console.log(high)
+//     console.log(low)
+//     if (high < low) {
 
-        console.log("HIGH OVER LOW")
+//         console.log("HIGH OVER LOW")
 
-        return 0;
+//         return 0;
 
-    } 
-    if (high === low) return low;
-    let mid = Math.floor((low + high) / 2)
+//     } 
+//     if (high === low) return low;
+//     let mid = Math.floor((low + high) / 2)
   
-    // Check if element (mid+1) is minimum element.
-    // Consider the cases like [3, 4, 5, 1, 2]
-    if (mid < high && arr[mid + 1] < arr[mid])
-      return mid + 1;
+//     // Check if element (mid+1) is minimum element.
+//     // Consider the cases like [3, 4, 5, 1, 2]
+//     if (mid < high && arr[mid + 1] < arr[mid])
+//       return mid + 1;
   
-    // Check if mid itself is minimum element
-    if (mid > low && arr[mid] < arr[mid - 1]) {
-      return mid;
-    }
+//     // Check if mid itself is minimum element
+//     if (mid > low && arr[mid] < arr[mid - 1]) {
+//       return mid;
+//     }
   
-    // Decide whether we need to go to left half or
-    // right half
-    if (arr[high] > arr[mid]) {
-      return findRotationCount(arr, low, mid - 1);
-    }
+//     // Decide whether we need to go to left half or
+//     // right half
+//     if (arr[high] > arr[mid]) {
+//       return findRotationCount(arr, low, mid - 1);
+//     }
   
-    return findRotationCount(arr, mid + 1, high);
-  }
+//     return findRotationCount(arr, mid + 1, high);
+//   }
   
   module.exports = findRotationCount
